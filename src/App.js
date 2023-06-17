148dea505f5a6a2ea2a33609e152aa68c34b164c
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-06-17 12:51:27 +0300

    added lazy loading pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import {useEffect} from 'react';
+import React, {useEffect, Suspense} from 'react';
 import {Routes, Route, NavLink} from 'react-router-dom';
 import {useDispatch} from 'react-redux'; 
-
-import GamesPage from 'pages/GamesPage';
-import GameDetailsPage from 'pages/GameDetailsPage';
-import LibraryPage from 'pages/LibraryPage';
-import RegisterPage from 'pages/RegisterPage';
-import LoginPage from 'pages/LoginPage';
-
 import {isUserLoggedIn} from 'utils/utils.js';
 import {setUserAction, setIsAuthAction} from 'redux/reducers/user/actions.js';
 import './App.css';
 
+const GamesPage = React.lazy(() => import('pages/GamesPage'));
+const GameDetailsPage = React.lazy(() => import('pages/GameDetailsPage'));
+const LibraryPage = React.lazy(() => import('pages/LibraryPage'));
+const RegisterPage = React.lazy(() => import('pages/RegisterPage'));
+const LoginPage = React.lazy(() => import('pages/LoginPage'));
 
 
 export default function App() {
@@ -27,13 +25,15 @@ export default function App() {
 
     return (
         <>
-            <Routes>
-                <Route path='/' element={<GamesPage />}/>
-                <Route path='/game/:id' element={<GameDetailsPage />}/>
-                <Route path='/library' element={<LibraryPage />}/>
-                <Route path='/register' element={<RegisterPage />}/>
-                <Route path='/login' element={<LoginPage />}/>
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='/' element={<GamesPage />}/>
+                    <Route path='/game/:id' element={<GameDetailsPage />}/>
+                    <Route path='/library' element={<LibraryPage />}/>
+                    <Route path='/register' element={<RegisterPage />}/>
+                    <Route path='/login' element={<LoginPage />}/>
+                </Routes>
+            </Suspense>
         </>
     );
 }

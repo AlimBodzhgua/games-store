d23681e4cd5c224224e933eac845a1acce502cc6
AlimBodzhgua <bodzhgua2015@mail.ru>
2025-06-04 19:37:05 +0300

    added keys to list, changed import

--- src/components/Sidebar/SidebarHeader.tsx
+++ src/components/Sidebar/SidebarHeader.tsx
@@ -1,7 +1,7 @@
 import { FC, memo } from 'react';
 import { useAction } from '@/hooks/useAction';
+import { Link } from '@/components/UI/Link/Link';
 import { sidebarHeaderList } from './sidebarList';
-import { Link } from '../UI/Link/Link';
 import classes from './sidebar.module.css';
 
 
@@ -11,7 +11,12 @@ export const SidebarHeader: FC = memo(() => {
 	const handleClick = () => setGenreAction(null);
 
 	return sidebarHeaderList.map((item) => (
-		<Link to='/' onClick={handleClick} className={classes.link}>
+		<Link
+			to='/'
+			onClick={handleClick}
+			className={classes.link}
+			key={item.path}
+		>
 			<item.Icon className={classes.icon}/>
 			{item.title}
 		</Link>

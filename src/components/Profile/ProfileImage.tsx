d1db4efc3de3fe51e754ba7c16d66dc41b1c5252
AlimBodzhgua <bodzhgua2015@mail.ru>
2025-06-11 18:14:36 +0300

    renamed handlers and classname added className props, added selectUserIamge selector

--- src/components/Profile/ProfileImage.tsx
+++ src/components/Profile/ProfileImage.tsx
@@ -1,25 +1,31 @@
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import { useAction } from '@/hooks/useAction';
 import { useAppSelector } from '@/hooks/redux';
+import { Button } from '@/components/UI/Button/Button';
+import { selectUserImage } from '@/redux/selectors/userSelectors';
 // @ts-ignore
 import ReactFileReader from 'react-file-reader';
-
+import classnames from 'classnames';
 import classes from './profile.module.css';
-import { Button } from '../UI/Button/Button';
 
-export const ProfileImage = () => {
-	const img = useAppSelector((state) => state.user.data?.img);
+interface ProfileImageProps {
+	className?: string;
+}
+
+export const ProfileImage: FC<ProfileImageProps> = (props) => {
+	const { className } = props;
+	const img = useAppSelector(selectUserImage);
 	const [selectedFile, setSelectedFile] = useState<string | null>(null);
 	const [isUploaded, setIsUploaded] = useState<boolean>(false);
 	const { changeImageAction } = useAction();
 
 	// @ts-ignore
-	const handleClick = (e) => {
+	const onUpload = (e) => {
 		setSelectedFile(e.base64);
 		setIsUploaded(true);
 	};
 
-	const handleSave = () => {
+	const onSave = () => {
 		if (selectedFile) {
 			changeImageAction(selectedFile);
 			setIsUploaded(false);
@@ -27,17 +33,17 @@ export const ProfileImage = () => {
 	};
 
 	return (
-		<div className={classes.profile__image}>
+		<div className={classnames(classes.ProfileImage, className)}>
 			<img className={classes.image} src={img} />
 			<div className={classes.upload__actions}>
-				<ReactFileReader base64 handleFiles={handleClick}>
+				<ReactFileReader base64 handleFiles={onUpload}>
 					<Button theme='white'>
 						upload image
 					</Button>
 				</ReactFileReader>
 				<Button
 					theme='blue'
-					onClick={handleSave}
+					onClick={onSave}
 					disabled={isUploaded ? false : true}
 				>
 					save image
--- src/redux/selectors/userSelectors.ts
+++ src/redux/selectors/userSelectors.ts
@@ -9,4 +9,5 @@ export const selectUserEmail = (state: StateSchema) => state.user.data?.email;
 export const selectUserLibrary = (state: StateSchema) => state.user.data?.library;
 export const selectUserId = (state: StateSchema) => state.user.data?.id;
 export const selectUserLogin = (state: StateSchema) => state.user.data?.login;
+export const selectUserImage = (state: StateSchema) => state.user.data?.img;
 
